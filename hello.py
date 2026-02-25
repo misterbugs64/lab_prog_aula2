4a0b5010365181fe254c037a7e3d84debff1f1df
mr_bugs <miguelbrennand@gmail.com>
2026-02-25 13:44:34 +0000

    nova versão

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-dia_da_semana = int(input("Digite um número da semana\nque são representados pelos números de 1 a 7:"))
-
+dia_da_semana = int(input("Digite um número da semana que são representados pelos números de 1 a 7: "))
+mês = int(input("informe o número do mês: "))
 
 match dia_da_semana:
-    case 1|2|3|4|5:
+    case 1|2|3|4|5 if mês == 5:
         print("é um dia da semana")
-    case 6|7:
+    case 6|7 if mês == 4:
         print("é um final de semana")
     case _:
-        print("dia invalido")
+        print("dia invalido ou mês invalido")
