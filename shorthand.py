63bdc09862e703a9235e5d20b6dfbe8b1ae113af
mr_bugs <miguelbrennand@gmail.com>
2026-02-25 14:00:25 +0000

    shorthand

---
+++ shorthand.py
@@ -0,0 +1,6 @@
+nota1 = float(input("coloque a primeira nota: "))
+nota2 = float(input("coleque a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+if media > 7.0: print(f"o aluno passou de ano com a media: {media}")
