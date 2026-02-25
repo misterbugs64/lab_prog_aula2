4664eeb315a55c6350e484bd2114ac5a2a0817d4
mr_bugs <miguelbrennand@gmail.com>
2026-02-25 13:40:25 +0000

    outro teste

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hello wolrd")
+dia_da_semana = int(input("Digite um número da semana\nque são representados pelos números de 1 a 7:"))
+
+
+match dia_da_semana:
+    case 1|2|3|4|5:
+        print("é um dia da semana")
+    case 6|7:
+        print("é um final de semana")
+    case _:
+        print("dia invalido")
